6e8c9a0c32b9e8bfff578c722a8c39425cf14f53
Bagus-sulaeman <58540806+Bagus-sulaeman@users.noreply.github.com>
2019-12-10 01:07:27 +0700

    file View
    Berada di View (tanpa folder)

---
+++ syarat.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Syarat dan ketentuan</title>
+        <link href="/css/app.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <h2>Syarat dan ketentuan penyewaan</h2>
+    <br>
+    <div class="media">
+  <div class="media-body">
+    <h4 class="mt-0">Aturan Sewa</h4>
+    <p>1. Penyewaan minimal 1 hari atau 1 x 24 jam.</p>
+    <p>2. Bla bla bla</p>
+  </div>
+</div>
+  <div class="media-body">
+    <h4 class="mt-0">Biaya Sewa</h4>
+    <p>1. Penyewaan 1 hari atau 1 x 24 jam sebesar Rp. 200.000,- .</p>
+    <p>2. Bla bla bla</p>
+  </div>
+</div>
+<br>
+<br>
+<a href="{{url('/daftar')}}" class="badge">Lihat daftar penyewa</a>
+<a href="{{url('/')}}" class="badge">Kembali ke halaman Home</a>
+    </body>
+</html>
