d4d9a21edd55d110693e63cd76cc77b5320e2bee
Bagus-sulaeman <58540806+Bagus-sulaeman@users.noreply.github.com>
2019-12-10 20:14:52 +0700

    Update RenterController.php
    Menambahkan fungsi select untuk menampilkan seluruh data pada table.

--- RenterController.php
+++ RenterController.php
@@ -14,72 +14,8 @@ class RenterController extends Controller
      */
     public function index()
     {
-        return view('renters.daftar');
+        $renter = Renter::all();
+        return view('renters.daftar',['renter' => $renter]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Renter  $tambah
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Renter $Renter)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Renter  $tambah
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Renter $Renter )
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Renter  $Renter 
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Renter $Renter )
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Renter  $Renter 
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Renter $Renter )
-    {
-        //
-    }
 }
