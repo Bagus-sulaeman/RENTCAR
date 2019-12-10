9eae367d9a9d3ac471ca9b90a46e077d6c3a6aff
Bagus-sulaeman <58540806+Bagus-sulaeman@users.noreply.github.com>
2019-12-10 20:03:48 +0700

    Update home.blade.php
    Pada fase ini halaman home (untuk menambahkan data) sudah bisa insert ke database namun hanya pada input nama, untuk input NIK, No. HP & Alamat masih belum bisa dikarenakan saat data ditambahkan, data menjadi NULL. Kemungkinan penyebab pada validasi di controller ControllerTambah@tambah

--- home.blade.php
+++ home.blade.php
@@ -10,26 +10,34 @@
     <h2>Tambah Data Penyewa</h2>
     <br><br>
     <form method="post" action="/">
-    {{ csrf_field() }}
+    {{ csrf_field() }}]
   <div class="form-group">
     <label for="nama">Nama</label>
-    <input type="text" class="form-control" id="nama" name="nama" placeholder="Isi Nama">
+    <input type="text" class="form-control" id="nama" name="nama" placeholder="Isi Nama" >
+    @if($errors->has('nama')) <div class="text-danger">{{ $errors->first('nama') }}</div>
+    @endif
   </div>
   <div class="form-group">
     <label for="nik">NIK</label>
-    <input type="text" class="form-control" id="nik" nama="nik" placeholder="Isi NIK">
+    <input type="text" class="form-control" id="nik" nama="nik" placeholder="Isi NIK" >
+    @if($errors->has('nik')) <div class="text-danger">{{ $errors->first('nik') }}</div>
+    @endif
   </div>
   <div class="form-group">
     <label for="phone">No. HP</label>
-    <input type="text" class="form-control" id="phone" nama="phone" placeholder="Isi Nomor Handphone">
+    <input type="text" class="form-control" id="phone" nama="phone" placeholder="Isi Nomor Handphone" >
+    @if($errors->has('phone')) <div class="text-danger">{{ $errors->first('phone') }}</div>
+    @endif
   </div>
   <div class="form-group">
     <label for="alamat">Alamat</label>
-    <input type="text" class="form-control" id="alamat" nama="alamat" placeholder="Isi Alamat">
+    <input type="text" class="form-control" id="alamat" nama="alamat" placeholder="Isi Alamat" >
+    @if($errors->has('alamat')) <div class="text-danger">{{ $errors->first('alamat') }}</div>
+    @endif
   </div>
   <button type="submit" class="btn btn-primary">Tambah data</button>
 </form>
-<br>
+<br><br>
 <a href="{{url('/daftar')}}" class="badge">Lihat daftar penyewa</a>
 <a href="{{url('/syarat')}}" class="badge">Lihat syarat dan ketentuan sewa</a>
     </body>
